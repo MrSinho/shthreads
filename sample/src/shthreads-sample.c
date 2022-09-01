3f7036b654b3df164ea58776b25f6c1b1ee3e88b
Anas Boustani <sino3791@gmail.com>
2022-09-01 04:40:16 +0200

    Initial commit

---
+++ sample/src/shthreads-sample.c
@@ -0,0 +1,14 @@
+#ifdef __cplusplus
+extern "C" {
+#endif//_cplusplus
+
+
+int main(void) {
+	
+	return 0;
+}
+
+#ifdef __cplusplus
+}
+#endif//_cplusplus
+
---
+++ shthreads/include/shthreads/shthreads.h
@@ -0,0 +1,16 @@
+#ifndef SH_THREADS_H
+#define SH_THREADS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif//__cplusplus
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif//_cplusplus
+
+#endif//SH_THREADS_H
